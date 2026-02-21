24d25d7aeb4d5dba186495e534f7c42e29ade933
AlponaKhan <alponakhan321@gmail.com>
2026-02-21 12:00:09 +0600

    modal added

--- product.js
+++ product.js
@@ -6,19 +6,59 @@ const loadAllCategory = () => {
         .then((data) => displayCategory(data))
 }
 
-const allProducts=()=>{
+const allProducts = () => {
     fetch("https://fakestoreapi.com/products")
-    .then((res)=>res.json())
-    .then((data)=>displayProducts(data))
+        .then((res) => res.json())
+        .then((data) => displayProducts(data))
+
 }
 
 const loadProducts = (ctgory) => {
-    // console.log(ctgory);
     const url = `https://fakestoreapi.com/products/category/${ctgory}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayProducts(data))
+        .then(data => {
+            displayProducts(data)
+        })
+
+
 }
+
+const loadProductDetails = async (id) => {
+    const url = `https://fakestoreapi.com/products/${id}`;
+    // console.log(url);
+    const res = await fetch(url);
+    const details = await res.json();
+    displayProductDetails(details);
+}
+
+const displayProductDetails = (product) => {
+    console.log(product);
+    const detailsMode = document.getElementById("details-container");
+    detailsMode.innerHTML = `
+    <div class="card bg-base-100  shadow-sm mx-auto">
+                    <div class="card-body">
+                        <h2 class="card-title font-bold text-xl">${product.title}</h2>
+                        <p class=" text-base">$${product.description}</p>
+                        <div class="flex">
+                        <p class="font-bold text-xl">$${product.price}</p>
+                        <div class="flex">
+                             <i class="fa-solid fa-star text-yellow-500"></i>
+                             <p class="font-bold text-xl">${product.rating.rate} <span>(${product.rating.count})</span></p>
+                        </div>
+                        </div>
+                            <button class="btn btn-primary flex-1">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                                Buy Now
+                            </button>
+                    </div>
+                </div>
+    `;
+    document.getElementById("my_modal_5").showModal();
+
+
+}
+
 const displayProducts = (products) => {
     const productsByCategory = document.getElementById("products-by-category");
     productsByCategory.innerHTML = "";
@@ -43,7 +83,7 @@ const displayProducts = (products) => {
                         <h2 class="card-title font-bold text-base">${product.title}</h2>
                         <p class="font-bold text-base">$${product.price}</p>
                         <div class="card-actions flex justify-between">
-                            <button class="btn flex-1">
+                            <button onClick="loadProductDetails(${product.id})" class="btn flex-1">
                                 <i class="fa-regular fa-eye"></i>
                                 Details
                             </button>
@@ -60,6 +100,14 @@ const displayProducts = (products) => {
     }
 }
 
+function allBtn(btn) {
+    const btnAll = document.querySelectorAll(".btn")
+    btnAll.forEach(btn => {
+        btn.classList.remove("active");
+    });
+    btn.classList.add("active");
+}
+
 const displayCategory = (category) => {
     const CategoryContainer = document.getElementById("category-container");
     // CategoryContainer.innerHTML = "";
@@ -67,9 +115,17 @@ const displayCategory = (category) => {
     for (let cate of category) {
         const btn = document.createElement("div");
         btn.innerHTML = `
-        <button class="btn btn-sm rounded-full hover:bg-blue-800 hover:text-white">${cate}</button>
+        <button id="active-cate" class="btn btn-sm rounded-full hover:bg-blue-800 hover:text-white">${cate}</button>
         `
-        btn.addEventListener("click", () => { loadProducts(cate) })
+        btn.addEventListener("click", (event) => {
+            const cateBtn = document.querySelectorAll(".btn");
+            cateBtn.forEach((btn) => {
+                btn.classList.remove("active");
+            })
+            event.target.classList.add("active");
+
+            loadProducts(cate)
+        })
 
 
         CategoryContainer.append(btn);
